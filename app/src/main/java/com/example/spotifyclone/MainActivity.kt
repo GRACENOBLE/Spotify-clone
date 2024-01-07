e2af917c2e20516a185edc81083aaec35a74985a
GRACENOBLE <gracenoble72@gmail.com>
2024-01-07 10:16:13 +0300

    Creating screens

--- app/src/main/java/com/example/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/example/spotifyclone/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.spotifyclone.ui.theme.SpotifyCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,18 +26,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpotifyCloneTheme {
-        Greeting("Android")
-    }
-}
+
